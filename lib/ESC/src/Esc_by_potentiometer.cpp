1ed0e9a883681902598fdd1b523f7b5c42ccd537
tanravikorn <tanravikorn@gmail.com>
2025-10-09 19:06:39 +0700

    update 1.0

--- lib/ESC/src/Esc_by_potentiometer.cpp
+++ lib/ESC/src/Esc_by_potentiometer.cpp
@@ -1,8 +1,6 @@
 #include "Esc_by_potentiometer.h"
 
-void CUHAR::ESC::potentiometer_esc(const int Poten_read)
+void CUHAR::ESC::potentiometer_esc(const int speed)
 {
-    const int speed = map(Poten_read, 0, 4095, min_pwm,max_pwm);
     Esc.writeMicroseconds(speed);
-    delay(200);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,24 @@
 #include "Esc_by_potentiometer.h"
 
 // put function declarations here:
-const int max_speed = 1940; //follow manual esc
-const int min_speed = 1100;
 const int escpin = 12;
 const int potentiometer_pin = 32;
+const int max_pwm = 1940;
+const int min_pwm = 1100;
 CUHAR::ESC myEsc(escpin);
 
 void setup() {
   Serial.begin(115200);
   delay(200);
-
 }
 
 void loop() {
   const int value = analogRead(potentiometer_pin);
-  Serial.printf("%d \n", value);
-  delay(500);
-  myEsc.potentiometer_esc(value);
-  delay(200);
+  const int speed = map(value, 0, 4095, min_pwm, max_pwm);
+  Serial.printf("%d \n", speed);
+  delay(50);
+  myEsc.potentiometer_esc(speed);
+  delay(50);
 
 }
 
