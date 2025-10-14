d49fcb1f44c809f3ba3c5f93d431ea1575106d76
tanravikorn <tanravikorn@gmail.com>
2025-10-14 23:14:40 +0700

    CurrentSensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include "MyEsc.h"
-
-// put function declarations here:
+//ESC part
 const int escpin = 12;
 const int potentiometer_pin = 32;
 const int max_pwm = 1940;
 const int min_pwm = 1100;
 CUHAR::ESC myEsc(escpin);
+//Current Part
+const float Sensitive = 0.04; //sensitive of ACS758 from data sheet 40 mV/A
+const int current_sensor_pin = 34;
+const float VCC = 3.3; //max voltage that connect with esp32 3.3V or 5V?
+float V_offset = VCC/2; // voltage while don't have current in Circuit
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -14,12 +20,20 @@ void setup() {
 }
 
 void loop() {
+  //ESC
   const int value = analogRead(potentiometer_pin);
   const int speed = map(value, 0, 4095, min_pwm, max_pwm);
-  Serial.printf("%d \n", speed);
-  delay(50);
+  Serial.printf("speed : %d \n", speed);
+  delay(10);
   myEsc.potentiometer_esc(speed);
-  delay(50);
+  delay(20);
+  //Current sensor
+  int raw_value = analogRead(current_sensor_pin); //0-4095
+  float V_out = (raw_value / (float) 4095 ) * VCC;
+  float current = (V_out - V_offset) / Sensitive;
+  Serial.printf("Current: %.3f\n", current);
+  delay(10);
+
 
 }
 
