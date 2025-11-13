74bd88cdcd4934fc00b95431ee6608b0f41e66ab
tanravikorn <tanravikorn@gmail.com>
2025-11-13 11:05:30 +0700

    change_sensitive_cureent_sensor

--- lib/current/src/Mycurrent.h
+++ lib/current/src/Mycurrent.h
@@ -6,9 +6,9 @@ namespace CUHAR
     class CurrentSensors
     {
         private:
-            const float Sensitive = 0.04;
+            const float Sensitive = 0.04 * (3.3 / 5.0); // because we use vcc = 3.3 so we need to change sensitive 
             const float VCC = 3.3; //max voltage that connect with esp32 3.3V or 5V?
-            float V_offset = VCC / 2; // voltage while don't have current in Circuit
+            const float V_offset = VCC / 2.0; // voltage while don't have current in Circuit
             int pin;
         public:
             CurrentSensors(const int &a) {
