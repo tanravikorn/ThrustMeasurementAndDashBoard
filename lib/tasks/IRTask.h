8dc2e40bc37cddc2267e25d49e52be773305c622
tanravikorn <tanravikorn@gmail.com>
2026-03-17 11:08:56 +0700

    (fixed)IRsensor

--- lib/tasks/IRTask.h
+++ lib/tasks/IRTask.h
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "globalData.h"
+#include "esp_timer.h"
 
 #define IR_SENSOR_PIN 13
 
@@ -13,31 +14,18 @@ void IRAM_ATTR isr()
     rev++;
 }
 
-uint64_t micros64()
-{
-    uint32_t now = micros(); // 32-bit 
-
-    if (now < last_micros32) {
-        overflow_count++;
-    }
-
-    last_micros32 = now;
-
-    return (overflow_count << 32) | now;
-}
 
 void IRSensorTask(void *pvParameters)
 {
     pinMode(IR_SENSOR_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(IR_SENSOR_PIN), isr, RISING);
 
-    uint64_t last_time = micros64();
+    int64_t last_time = esp_timer_get_time(); 
 
     for (;;)
     {
-
-        uint64_t current_time = micros64(); //return 64-bits
-        uint64_t time_diff = current_time - last_time; // µs
+        int64_t current_time = esp_timer_get_time(); 
+        int64_t time_diff = current_time - last_time;
 
         uint32_t count;
         noInterrupts();
@@ -45,14 +33,14 @@ void IRSensorTask(void *pvParameters)
         rev = 0;
         interrupts();
 
-        if (time_diff > 0)
-        {
+        if (count == 0) {
+            globalData.rpm = 0;
+        } else if (time_diff > 0) {
             float rpm = (count * 60000000.0f) / (float)time_diff;
             globalData.rpm = (int)rpm;
         }
 
         last_time = current_time;
-
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
